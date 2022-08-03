3ea6a8ffaf627212463d8f323a844fd53cdd30c9
hlimach <hramzan.bscs18seecs@seecs.edu.pk>
2022-08-03 16:50:40 +0300

    Added avg tile feature extraction and analysis.

--- utils.py
+++ utils.py
@@ -1,17 +1,27 @@
 import os
 import cv2
 import numpy as np
-from skimage.feature import greycomatrix, greycoprops
+
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import figure
 
+from fast_glcm import *
+
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 COLOR = (255,0,0)
+FEATURES = {1: 'mean', 
+            2: 'std', 
+            3: 'contrast', 
+            4: 'dissimilarity', 
+            5: 'homogeneity', 
+            6: 'asm',
+            7: 'energy',
+            8: 'max_',
+            9: 'entropy'}
 
 def load_pngs_dict(path):
     '''
-    reads images from given path and stores along with their id in a dict
-    returns: dict of id and image
+    use path and create dict of id and images
     '''
     imgs_ids = {}
     
@@ -25,31 +35,49 @@ def load_pngs_dict(path):
 
     return imgs_ids
 
-def get_tiles(img, n=10, m=5):
+def get_crop_image(img, h, w):
     '''
-    Crops image to make height divisible by 5 and width by 10 (default)
-    Generates tiles from cropped image using the n and m values
-    Generates masked image with tiles printed for visual guide
-    returns: cropped image, dict of tiles with their id, masked image showing tiles and ids
+    crop img to fit tiling requirement
     '''
-
     height, width = img.shape[:2]
 
-    new_width = width - (width % n)
-    new_height = height - (height % m)
+    new_width = width - (width % w)
+    new_height = height - (height % h)
 
     img = img[0:new_height, 0:new_width]
 
-    tile_w = int(new_width/n)
-    tile_h = int(new_height/m)
+    return img
+
+def get_tiles_by_grid(img, r=5, c=10):
+    '''
+    Parameters
+    ----------
+    img: shape=(h,w,ch), dtype=np.uint8
+        input image
+    r: int
+        number of rows in grid
+    c: int
+        number of columns in grid
+
+    Returns
+    -------
+    Cropped image, shape = (h,w,ch)
+    Dict of tiles generated, {id: tile}
+    Image masked with tiles and ids, shape = (h,w,ch)
+    '''
+
+    img = get_crop_image(img, r, c)
+    height, width = img.shape[:2]
+
+    tile_w = int(width/c)
+    tile_h = int(height/r)
 
     mask = img.copy()
-
     id = 0
     tiles = {}
 
-    for y in range(0, new_height, tile_h):
-        for x in range(0, new_width, tile_w):
+    for y in range(0, height, tile_h):
+        for x in range(0, width, tile_w):
             cv2.rectangle(mask, pt1=(x,y), pt2=(x+tile_w-1,y+tile_h-1), color=COLOR, thickness=1)
             cv2.putText(mask, str(id), (x,y+tile_h), FONT, 1, COLOR, 3, cv2.LINE_AA)
             tile = img[y:y+tile_h, x:x+tile_w]
@@ -58,118 +86,89 @@ def get_tiles(img, n=10, m=5):
 
     return img, tiles, mask
 
-def glcmXbyXWinScan(sarraster, windowSize):
-    #Create rasters to receive texture and define filenames
-    contrastraster = np.zeros((sarraster.shape[0], sarraster.shape[1]), dtype = float)
-    contrastraster[:] = 0.0
-    
-    dissimilarityraster = np.zeros((sarraster.shape[0], sarraster.shape[1]), dtype = float)
-    dissimilarityraster[:] = 0.0
-    
-    #homogeneityraster = np.copy(sarraster)
-    homogeneityraster = np.zeros((sarraster.shape[0], sarraster.shape[1]), dtype = float)
-    homogeneityraster[:] = 0.0
-    
-    #energyraster = np.copy(sarraster)
-    energyraster = np.zeros((sarraster.shape[0], sarraster.shape[1]), dtype = float)
-    energyraster[:] = 0.0
-    
-    #correlationraster = np.copy(sarraster)
-    correlationraster = np.zeros((sarraster.shape[0], sarraster.shape[1]), dtype = float)
-    correlationraster[:] = 0.0
-    
-    #ASMraster = np.copy(sarraster)
-    ASMraster = np.zeros((sarraster.shape[0], sarraster.shape[1]), dtype = float)
-    ASMraster[:] = 0.0
-    
-    
-    for i in range(sarraster.shape[0]):
-        for j in range(sarraster.shape[1]):
-            if i < windowSize or j < windowSize:
-                continue
-            if i > (contrastraster.shape[0] - windowSize) or j > (contrastraster.shape[1] - windowSize):
-                continue
-            
-            # Define size of moving window
-            glcm_window = sarraster[i-windowSize: i+windowSize, j-windowSize : j+windowSize]
-            
-            # Calculate GLCM and textures
-            glcm = greycomatrix(glcm_window, [1], [0],  symmetric = True, normed = True )
-    
-            # Calculate texture and write into raster where moving window is centered
-            contrastraster[i,j]      = greycoprops(glcm, 'contrast')
-            dissimilarityraster[i,j] = greycoprops(glcm, 'dissimilarity')
-            homogeneityraster[i,j]   = greycoprops(glcm, 'homogeneity')
-            energyraster[i,j]        = greycoprops(glcm, 'energy')
-            correlationraster[i,j]   = greycoprops(glcm, 'correlation')
-            ASMraster[i,j]           = greycoprops(glcm, 'ASM')
-            glcm = None
-            glcm_window = None
-            
-    #Normalization use when only needed   
-    contrastraster = 255.0 * normalize(contrastraster)
-    contrastraster = contrastraster.astype(int)
-    
-    dissimilarityraster = 255.0 * normalize(dissimilarityraster)
-    dissimilarityraster = dissimilarityraster.astype(int)
-    
-    homogeneityraster = 255.0 * normalize(homogeneityraster)
-    homogeneityraster = homogeneityraster.astype(int)
-    
-    energyraster = 255.0 * normalize(energyraster)
-    energyraster = energyraster.astype(int)
-    
-    correlationraster = 255.0 * normalize(correlationraster)
-    correlationraster = correlationraster.astype(int)
-    
-    ASMraster = 255.0 * normalize(ASMraster)
-    ASMraster = ASMraster.astype(int)
+def get_tiles_by_pixels(img, h=100, w=100):
+    '''
+    Parameters
+    ----------
+    img: shape=(h,w,ch), dtype=np.uint8
+        input image
+    h: int
+        height of tile in pixels
+    w: int
+        width of tile in pixels
 
-    return contrastraster, dissimilarityraster, homogeneityraster, energyraster, correlationraster, ASMraster
+    Returns
+    -------
+    Cropped image, shape = (h,w,ch)
+    Dict of tiles generated, {id: tile}
+    Image masked with tiles and ids, shape = (h,w,ch)
+    '''
 
+    img = get_crop_image(img, h, w)
+    height, width = img.shape[:2]
+    mask = img.copy()
 
-def normalize(arrayX):
-    for i in range(arrayX.shape[0]):
-        for j in range(arrayX.shape[1]):
-            arrayX[i,j] = ((arrayX[i,j] - arrayX.min()) / (arrayX.max() - arrayX.min()))
-            
-    return arrayX
+    id = 0
+    tiles = {}
 
+    for y in range(0, height, h):
+        for x in range(0, width, w):
+            cv2.rectangle(mask, pt1=(x,y), pt2=(x+w-1,y+h-1), color=COLOR, thickness=1)
+            cv2.putText(mask, str(id), (x,y+h), FONT, 1, COLOR, 3, cv2.LINE_AA)
+            tile = img[y:y+h, x:x+w]
+            tiles[id] = tile
+            id = id + 1
 
-def display_window_glcm(img):
+    return img, tiles, mask
+
+def calc_glcm_features(img):
     img = img[:,:,0]
 
-    windowSz = 15       # change the window size based on need
-    contrastScan, dissimilarityScan, homogeneityScan, energyScan, correlationScan, ASMScan = glcmXbyXWinScan(img, windowSz)
+    mean = fast_glcm_mean(img)
+    std = fast_glcm_std(img)
+    contrast = fast_glcm_contrast(img)
+    dissimilarity = fast_glcm_dissimilarity(img)
+    homogeneity = fast_glcm_homogeneity(img)
+    asm, energy = fast_glcm_ASM(img)
+    max_ = fast_glcm_max(img)
+    entropy = fast_glcm_entropy(img)
 
+    glcm_features = {}
+    for key in FEATURES:
+        glcm_features[FEATURES[key]] = eval(FEATURES[key])
+
+    return glcm_features
+
+def display_glcm_features(glcm_features, id):
     fig = plt.figure(figsize=(22, 18))
-    texturelist = {1: 'contrast', 2: 'dissimilarity', 3: ' homogeneity', 4: 'energy', 5: 'correlation', 6: 'ASM'}
-    for key in texturelist:
-        ax = plt.subplot(3,2,key)
-        plt.axis('off')
-        ax.set_title(texturelist[key])
-        plt.imshow(eval(texturelist[key] + "Scan"), cmap = 'gray')
+    fig.suptitle(id)
 
+    for key in FEATURES:
+        ax = plt.subplot(3,3,key)
+        ax.set_title(FEATURES[key])
+        plt.imshow(glcm_features[FEATURES[key]], cmap = 'gray')
     plt.show()
 
+def get_avg_features(glcm_features):
+    avg_features = {}
+    for key in glcm_features:
+        avg_features[key] = np.average(glcm_features[key])
+    return avg_features
 
-def display_glcm(img):
-    img = img[:,:,0]
+def calc_avg_tile_features(tiles):
+    avg_tile_features = {}
+    for id in tiles:
+        glcm_features = calc_glcm_features(tiles[id])
+        avg_tile_features[id] = get_avg_features(glcm_features)
+    return avg_tile_features
 
-    glcm = greycomatrix(img, [1], [0],  symmetric = True, normed = True )
-    contrastraster = greycoprops(glcm, 'contrast')
-    dissimilarityraster = greycoprops(glcm, 'dissimilarity')
-    homogeneityraster = greycoprops(glcm, 'homogeneity')
-    energyraster = greycoprops(glcm, 'energy')
-    correlationraster = greycoprops(glcm, 'correlation')
-    ASMraster = greycoprops(glcm, 'ASM')
-
+def print_feature_graph(avg_tile_features):
     fig = plt.figure(figsize=(22, 18))
-    texturelist = {1: 'contrast', 2: 'dissimilarity', 3: ' homogeneity', 4: 'energy', 5: 'correlation', 6: 'ASM'}
-    for key in texturelist:
-        ax = plt.subplot(3,2,key)
-        plt.axis('off')
-        ax.set_title(texturelist[key])
-        plt.imshow(eval(texturelist[key] + "raster"), cmap = 'gray')
 
+    for key in FEATURES:
+        ax = plt.subplot(3,3,key)
+        ax.set_title(FEATURES[key])
+        x = [x for x in avg_tile_features]
+        y = [avg_tile_features[x][FEATURES[key]] for x in avg_tile_features]
+        plt.scatter(x, y)
     plt.show()
